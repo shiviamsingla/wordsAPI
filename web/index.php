55065dd26433bc163f1e63378f0ccfb8f7e1cd25
Shiviam Singla <49753664+shiviamsingla@users.noreply.github.com>
2021-01-01 16:50:12 +0530

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Random Words API</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	Random Words API is created by shiviamsingla<br>
+	pass no{1,2} parameter<br>
+	<a href="words.php">Random English Words</a><br>
+	<a href="paragraphs.php">Random English Paragraphs</a><br>
+	<a href="punjabi_words.php">Random Punjabi Words</a><br>
+</body>
+</html>
